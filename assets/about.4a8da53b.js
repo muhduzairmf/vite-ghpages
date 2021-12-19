12f2376d3b215d4ea32f87c10cb0b2f29133c29e
muhduzairmf <uzair.developer@outlook.com>
2021-12-19 17:48:31 +0800

    first init

---
+++ assets/about.4a8da53b.js
@@ -0,0 +1,4 @@
+import"./style.7f2dd081.js";document.querySelector("#app").innerHTML=`
+  <h1>Hello Vite!</h1>
+  <a href="../">Home</a>
+`;
---
+++ assets/main.1b5b28dc.js
@@ -0,0 +1,4 @@
+import"./style.7f2dd081.js";document.querySelector("#app").innerHTML=`
+  <h1>Hello Vite!</h1>
+  <a href="about/">About Us</a>
+`;
